a76219e8f3500e8c1f0916aca43e7555725ede19
Andy Fragen <andy@thefragens.com>
2021-05-18 09:31:11 -0700

    update for adding custom icon to Freemius messaging

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,8 @@
 [unreleased]
 
+#### 1.0.1 / 2021-05-18
+* ensure custom icon shows in update notice from Freemius
+
 #### 1.0.0 / 2021-05-11
 * migrate update, install, REST API, WP-CLI, remote management code over
 * add Zipfile_API code over
--- src/Git_Updater_PRO/GUP_Freemius.php
+++ src/Git_Updater_PRO/GUP_Freemius.php
@@ -83,5 +83,15 @@ class GUP_Freemius {
 			// Signal that SDK was initiated.
 			do_action( 'gup_fs_loaded' );
 		}
+		gup_fs()->add_filter( 'plugin_icon', [ $this, 'add_icon' ] );
+	}
+
+	/**
+	 * Add custom plugin icon to update notice.
+	 *
+	 * @return string
+	 */
+	public function add_icon() {
+		return dirname( __DIR__, 2 ) . '/assets/icon.svg';
 	}
 }
