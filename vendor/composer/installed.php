8e798d392b5646683fb69c15ac1d80b8650a25cd
Andy Fragen <andy@thefragens.com>
2021-06-14 20:28:31 -0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,43 +1,43 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bdb313b3d9af253a94daf1d8feed17d1c4da08ac',
+        'name' => 'afragen/git-updater-pro',
+        'dev' => false,
     ),
-    'reference' => 'abd7164e338bc22d95b292122d0203343702fe42',
-    'name' => 'afragen/git-updater-pro',
-  ),
-  'versions' => 
-  array (
-    'afragen/git-updater-pro' => 
-    array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'abd7164e338bc22d95b292122d0203343702fe42',
+    'versions' => array(
+        'afragen/git-updater-pro' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bdb313b3d9af253a94daf1d8feed17d1c4da08ac',
+            'dev_requirement' => false,
+        ),
+        'afragen/singleton' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../afragen/singleton',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'be8e3c3b3a53ba30db9f77f5b3bcf2d5e58ed9c0',
+            'dev_requirement' => false,
+        ),
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.4.2',
+            'version' => '2.4.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'reference' => '84a9be4717effd7697a217e0d931f48ae0d2ecc6',
+            'dev_requirement' => false,
+        ),
     ),
-    'afragen/singleton' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'be8e3c3b3a53ba30db9f77f5b3bcf2d5e58ed9c0',
-    ),
-    'freemius/wordpress-sdk' => 
-    array (
-      'pretty_version' => '2.4.2',
-      'version' => '2.4.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '84a9be4717effd7697a217e0d931f48ae0d2ecc6',
-    ),
-  ),
 );
